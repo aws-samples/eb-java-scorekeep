4d6cce58b924faaad538f54e40ae32230da5e140
Michael Wunderlich <mwunderl@example.com>
2016-11-01 18:32:40 +0000

    working default API path for non CORS configuration

--- public/app/scorekeep.js
+++ public/app/scorekeep.js
@@ -1,2 +1,10 @@
 var module = angular.module("scorekeep", ["ngRoute","ngResource"]);
-module.value('api', 'http://XXXXX.elasticbeanstalk.com/api/');
+/* API running at /api on the same domain name and port (no CORS) */
+  module.value('api', '/api/');
+
+/* API running on Elastic Beanstalk, frontend running locally or
+   on a different domain (CORS) */
+// module.value('api', 'http://XXXXX.elasticbeanstalk.com/api/');
+
+/* API running locally on port 5000, frontend on port 8080 (CORS) */
+// module.value('api', 'http://localhost:5000/api/');
