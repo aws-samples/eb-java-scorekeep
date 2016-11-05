71fe8ad304883cbb49600d86ec2f1e53d529adbc
Michael Wunderlich <mwunderl@amazon.com>
2016-11-04 23:33:00 -0700

    q state refresh and moves, prevent q'ing multiple moves

--- public/app/gameController.js
+++ public/app/gameController.js
@@ -1,50 +1,74 @@
 var module = angular.module('scorekeep');
 module.controller('GameController', Game);
-function Game($scope, $http, $interval, $routeParams, SessionService, UserService, GameService, GameCollection, RulesService, StateService, api) {
+function Game($q, $scope, $http, $interval, $routeParams, SessionService, UserService, GameService, GameCollection, RulesService, StateService, api) {
   $scope.game = new GameService;
   $scope.state = new StateService; // game state object
   $scope.gamestate = []; // game state as Array
+  $scope.moving = 0;
 
   $scope.playgame = function(){
-    GetGame = $scope.game.$get({ sessionid: $routeParams.sessionid, id: $routeParams.gameid });
-    GetState = GetGame.then(function(){
-      currentstate = $scope.game.states[$scope.game.states.length-1];
-      return $scope.state.$get({ sessionid: $routeParams.sessionid, gameid: $routeParams.gameid, id: currentstate});
-    })
-    GetState.then(function(result){
-      $scope.gamestate = $scope.state.state.split('');
+    return $q(function(resolve, reject) {
+      GetGame = $scope.game.$get({ sessionid: $routeParams.sessionid, id: $routeParams.gameid });
+      GetState = GetGame.then(function(){
+        currentstate = $scope.game.states[$scope.game.states.length-1];
+        return $scope.state.$get({ sessionid: $routeParams.sessionid, gameid: $routeParams.gameid, id: currentstate});
+      })
+      SetState = GetState.then(function(result){
+        $scope.gamestate = $scope.state.state.split('');
+        resolve();
+      });
     });
   }
-  $scope.playgame();
+  $scope.promise = $scope.playgame();
   $scope.interval = $interval(function(){
-    $scope.playgame();
+    $scope.promise.then(function() {
+      $scope.promise = $scope.playgame();
+    })
   }, 5000);
 
   $scope.move = function(cellid){
-    $scope.gamestate = $scope.state.state.split('');
-    move = ""
-    if ( $scope.gamestate[cellid] != " " ) {
+    if ( $scope.moving == 1 ) {
       return;
     }
-    if ($scope.gamestate[0] == "X") {
-      $scope.gamestate[cellid] = "X";
-      $scope.gamestate[0] = "O";
-      move = "X" + cellid;
-    } else {
-      $scope.gamestate[cellid] = "O";
-      $scope.gamestate[0] = "X";
-      move = "O" + cellid;
-    }
-    PostMove = $http.post(api + 'move/' + $routeParams.sessionid + "/" + $routeParams.gameid + "/" + $routeParams.userid, move);
-    GetGame = PostMove.then(function(){
-      return $scope.game.$get({ sessionid: $routeParams.sessionid, id: $routeParams.gameid });
-    })
-    GetState = GetGame.then(function(GetGameResult){
-      stateid = $scope.game.states[$scope.game.states.length-1];
-      return $scope.state.$get({ sessionid: $routeParams.sessionid, gameid: $routeParams.gameid, id: stateid});
-    });
-    GetState.then(function(){
-      $scope.gamestate = $scope.state.state.split('');
+    $scope.moving = 1;
+    $scope.promise.then(function(){
+      $scope.promise = $q(function(resolve,reject){
+        console.log("MOVE on cell " + cellid);
+        $scope.gamestate = $scope.state.state.split('');
+        move = ""
+        // move is invalid
+        if ( $scope.gamestate[cellid] != " " ) {
+          return;
+        }
+        // temporarily update game board and determine move
+        if ($scope.gamestate[0] == "X") {
+          $scope.gamestate[cellid] = "X";
+          $scope.gamestate[0] = "O";
+          move = "X" + cellid;
+        } else {
+          $scope.gamestate[cellid] = "O";
+          $scope.gamestate[0] = "X";
+          move = "O" + cellid;
+        }
+        // send move
+        PostMove = $http.post(api + 'move/' + $routeParams.sessionid + "/" + $routeParams.gameid + "/" + $routeParams.userid, move);
+        // get new game state
+        GetGame = PostMove.then(function(){
+          return $scope.game.$get({ sessionid: $routeParams.sessionid, id: $routeParams.gameid });
+        })
+        GetState = GetGame.then(function(GetGameResult){
+          stateid = $scope.game.states[$scope.game.states.length-1];
+          return $scope.state.$get({ sessionid: $routeParams.sessionid, gameid: $routeParams.gameid, id: stateid});
+        });
+        // update game board
+        GetState.then(function(){
+          $scope.gamestate = $scope.state.state.split('');
+          $scope.moving = 0;
+          resolve();
+        });
+
+      });
+
     });
   }
 
