49dd98f2d97d87613d2cbaebfeb20e2c1ba0e562
Michael Wunderlich <mwunderl@example.com>
2017-01-16 20:08:27 +0000

    cleanup

--- public/app/scorekeep.js
+++ public/app/scorekeep.js
@@ -1,14 +1,15 @@
 var module = angular.module("scorekeep", ["ngRoute","ngResource"]);
 /* API running at /api on the same domain name and port (no CORS) */
-//  module.value('api', '/api/');
+module.value('api', '/api/');
 
 /* API running on Elastic Beanstalk, frontend running locally or
    on a different domain (CORS) */
 // module.value('api', 'http://XXXXX.elasticbeanstalk.com/api/');
 
 /* API running locally on port 5000, frontend on port 8080 (CORS) */
- module.value('api', '/api/');
- /* Cognito User pool */
- module.value('cognitoUserPoolId', 'us-east-1_AbCd12345');
- module.value('cognitoClientId', 'abcdef123456ghijklmnopqr7');
- module.value('cognitoRegion', 'us-east-1');
+// module.value('api', 'http://localhost:5000/api/');
+
+/* Cognito User pool */
+module.value('cognitoUserPoolId', 'us-east-1_AbCd12345');
+module.value('cognitoClientId', 'abcdef123456ghijklmnopqr7');
+module.value('cognitoRegion', 'us-east-1');
