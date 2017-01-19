69a53a11991ab8640bbdf49c2b6a0a8e1af7a08b
Michael Wunderlich <mwunderl@example.com>
2017-01-19 19:15:14 +0000

    add Beanstalk plugin

--- src/main/java/scorekeep/WebConfig.java
+++ src/main/java/scorekeep/WebConfig.java
@@ -4,6 +4,7 @@ import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 import com.amazonaws.xray.plugins.EC2Plugin;
+import com.amazonaws.xray.plugins.ElasticBeanstalkPlugin;
 import com.amazonaws.xray.strategy.sampling.DefaultSamplingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,7 +26,7 @@ public class WebConfig {
   }
 
   static {
-    AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin());
+    AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ElasticBeanstalkPlugin());
 
     URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
     builder.withSamplingStrategy(new DefaultSamplingStrategy(ruleFile));
