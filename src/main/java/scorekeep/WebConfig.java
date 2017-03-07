425141ad0555ee8d3083fc1fd0ba98ef8f3e884f
Michael Wunderlich <mwunderl@amazon.com>
2017-03-06 18:33:21 -0800

    update X-Ray SDK for Java to 1.0.5-beta

--- src/main/java/scorekeep/WebConfig.java
+++ src/main/java/scorekeep/WebConfig.java
@@ -5,7 +5,7 @@ import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 import com.amazonaws.xray.plugins.EC2Plugin;
 import com.amazonaws.xray.plugins.ElasticBeanstalkPlugin;
-import com.amazonaws.xray.strategy.sampling.DefaultSamplingStrategy;
+import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,7 +30,7 @@ public class WebConfig {
     AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ElasticBeanstalkPlugin());
 
     URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
-    builder.withSamplingStrategy(new DefaultSamplingStrategy(ruleFile));
+    builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
 
     AWSXRay.setGlobalRecorder(builder.build());
   }
