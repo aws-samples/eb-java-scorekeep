abb9be443ba814c23a3e91fcbbdd4198e2bcfc1b
Michael Wunderlich <mwunderl@amazon.com>
2017-07-06 00:59:56 +0000

    record build error in cause block

--- bin/xray_error.py
+++ bin/xray_error.py
@@ -2,14 +2,21 @@ import time
 import json
 import socket
 import sys
+import binascii
+import os
 
 SEGMENT_DOC = json.loads(sys.argv[1])
-ERROR = { "error": sys.argv[2] }
+EXCEPTION_ID = binascii.b2a_hex(os.urandom(8))
+WORKING_DIRECTORY = "/var/app/current"
+PATHS = "/var/app/current/src/main/java/scorekeep/"
+LOG = sys.argv[2]
+MESSAGE = LOG.split('* What went wrong:')[0]
+ERROR = { "working_directory": WORKING_DIRECTORY, "paths": [ PATHS ], "exceptions": [ { "id": EXCEPTION_ID, "message": MESSAGE } ] }
 del SEGMENT_DOC["in_progress"]
 END_TIME = time.time()
 SEGMENT_DOC["end_time"] = END_TIME
 SEGMENT_DOC["error"] = True
-SEGMENT_DOC["annotations"] = ERROR
+SEGMENT_DOC["cause"] = ERROR
 
 HEADER=json.dumps({"format": "json", "version": 1})
 TRACE_DATA = HEADER + "\n" + json.dumps(SEGMENT_DOC)
