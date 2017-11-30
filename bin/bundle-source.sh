26fcd97e7d96830b91b66d2aab47623750006344
Michael Wunderlich <mwunderl@amazon.com>
2017-11-29 22:29:22 -0800

    bundle script

--- bin/bundle-source.sh
+++ bin/bundle-source.sh
@@ -1,2 +1,2 @@
-git archive --format=zip HEAD > eb-java-scorekeep-v1.zip
-unzip -l eb-java-scorekeep-v1.zip 
+git archive --format=zip HEAD > eb-java-scorekeep-lambda-worker-v1.zip
+unzip -l eb-java-scorekeep-lambda-worker-v1.zip 
