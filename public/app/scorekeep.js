f440af0319d4d6a9eb192707aa5498d055847e18
riceshark <enotpetya@gmail.com>
2018-11-04 17:19:49 +0300

    .

--- public/app/scorekeep.js
+++ public/app/scorekeep.js
@@ -7,4 +7,4 @@ var module = angular.module("scorekeep", ["ngRoute", "ngResource"]);
  module.value('api', 'http://Scorekeep-env.sdap22vsfv.us-east-1.elasticbeanstalk.com/api/');
 
 /* API running locally on port 5000, frontend on port 8080 (CORS) */
- //module.value('api', 'http://localhost:5000/api/');
+ //module.value('api', 'http://loccdсalhost:5000/api/');
