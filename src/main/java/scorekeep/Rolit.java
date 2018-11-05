c98ae28b96a9792ab774bd8e74e93bd81944ce83
AlmazSamatov <samatov.almaz@yandex.ru>
2018-11-05 23:05:55 +0300

    fixed bug with indices

--- src/main/java/scorekeep/Rolit.java
+++ src/main/java/scorekeep/Rolit.java
@@ -410,7 +410,7 @@ public class Rolit {
         char[][] matrix = new char[8][8];
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                matrix[i][j] = state[(i * 8) + j + 1];
+                matrix[i][j] = state[(i * 8) + j + 2];
             }
         }
         return matrix;
@@ -422,7 +422,7 @@ public class Rolit {
         res[1] = c2;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                res[(i * 8) + j + 1] = matrix[i][j];
+                res[(i * 8) + j + 2] = matrix[i][j];
             }
         }
         return res;
