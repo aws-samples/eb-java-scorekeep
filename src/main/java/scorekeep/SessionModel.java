1925256e8d4c48e16a8977e2c73ab5375cc444bd
Michael Wunderlich <mwunderl@amazon.com>
2020-04-08 20:58:45 -0700

    automated deployment, portability

--- src/main/java/scorekeep/SessionModel.java
+++ src/main/java/scorekeep/SessionModel.java
@@ -3,6 +3,8 @@ package scorekeep;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig.TableNameOverride;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 
 import java.util.List;
@@ -12,7 +14,8 @@ public class SessionModel {
   /** AWS SDK credentials. */
   private AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
         .build();
-  private DynamoDBMapper mapper = new DynamoDBMapper(client);
+  private DynamoDBMapperConfig mapperConfig = new DynamoDBMapperConfig(new TableNameOverride(System.getenv("SESSION_TABLE")));
+  private DynamoDBMapper mapper = new DynamoDBMapper(client, mapperConfig);
 
   public void saveSession(Session session) {
     try {
