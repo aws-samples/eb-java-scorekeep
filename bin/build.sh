a7a73cf83fe8a902cc98b017701a5ec88bf61134
Michael Wunderlich <mwunderl@amazon.com>
2022-04-20 12:18:31 -0700

    build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,8 @@
 SEGMENT=$(python bin/xray_start.py)
-./gradlew build --quiet --stacktrace &> /var/log/gradle.log; GRADLE_RETURN=$?
+./gradlew build --quiet --stacktrace &> gradle.log; GRADLE_RETURN=$?
 if (( GRADLE_RETURN != 0 )); then 
   echo "Gradle failed with exit status $GRADLE_RETURN" >&2
-  python bin/xray_error.py "$SEGMENT" "$(cat /var/log/gradle.log)"
+  python bin/xray_error.py "$SEGMENT" "$(cat gradle.log)"
   exit 1
 fi
 python bin/xray_success.py "$SEGMENT"
